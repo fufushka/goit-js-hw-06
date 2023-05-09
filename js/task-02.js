c157ef36a2a5a0e046b23f5dc05b0f35f1af0b2d
fufushka <dimaukropdred@gmail.com>
2023-05-09 22:43:04 +0300

    a

--- js/task-02.js
+++ js/task-02.js
@@ -1,19 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const list = document.querySelector("#ingredients")
-  
- ingredients.forEach(ingridient  => {
-   const listEl = document.createElement("li")
-   listEl.textContent = ingridient
-   listEl.classList.add("item")
-   list.append(listEl)
-   console.log()
+const list = document.querySelector("#ingredients");
 
- });
+const markup = ingredients.forEach((ingridient) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = ingridient;
+  listEl.classList.add("item");
+});
+list.append(...markup);
+console.log();
